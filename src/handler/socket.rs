0af852c2efeb824216b9306ca930fb0d6b963006
ganda wenang sani <wenang.sani@gmail.com>
2021-03-18 23:16:38 +0800

    add socket on connect and disconnect

--- src/handler/socket.rs
+++ src/handler/socket.rs
@@ -1,31 +1,43 @@
 use crate::web::{Request, Response, Error};
 
 use actix::{Actor, StreamHandler};
-use actix_web_actors::ws;
+use actix_web_actors::ws::{WebsocketContext, Message, ProtocolError, start};
 
 /// Define HTTP actor
 struct MyWs;
 
 impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
+    type Context = WebsocketContext<Self>;
 }
 
 /// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+impl StreamHandler<Result<Message, ProtocolError>> for MyWs {
+
+    // On socket connect
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        println!("Connected");
+    }
+
+    // On socket disconnect
+    fn finished(&mut self, _ctx: &mut Self::Context) {
+        println!("Disconnected");
+    }
+    
+    // Handle socket
+    fn handle(&mut self, msg: Result<Message, ProtocolError>, ctx: &mut Self::Context) {
         println!("{:#?}", msg);
         match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => {
+            Ok(Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(Message::Text(text)) => {
                 return ctx.text(text);
             },
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(Message::Binary(bin)) => ctx.binary(bin),
             _ => (),
         }
     }
 }
 
 pub async fn main(req: Request, stream: actix_web::web::Payload) -> Result<Response, Error> {
-    let resp = ws::start(MyWs {}, &req, stream);
+    let resp = start(MyWs {}, &req, stream);
     return resp;
 }
