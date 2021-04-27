7daef787a786f390a6be1920f8852f418517093c
ganda wenang sani <wenang.sani@gmail.com>
2021-04-28 00:02:11 +0800

    repair socket

--- src/handler/socket.rs
+++ src/handler/socket.rs
@@ -1,31 +1,73 @@
 use crate::web::{Request, Response, Error};
 
-use actix::{Actor, StreamHandler};
-use actix_web_actors::ws::{WebsocketContext, Message, ProtocolError, start};
+use actix::{AsyncContext, Actor, StreamHandler};
+use actix_web_actors::ws::{WebsocketContext, Message, ProtocolError};
+use uuid::Uuid;
 
-/// Define HTTP actor
-struct MyWs;
+// Define HTTP actor
+#[derive(Clone, Debug)]
+pub struct WsConn {
+    id: Uuid
+}
 
-impl Actor for MyWs {
+impl WsConn {
+
+    // Create new socket connection
+    fn new() -> WsConn {
+
+        return WsConn {
+            id: Uuid::new_v4()
+        };
+    }
+
+    fn send(&mut self) {
+
+        println!("self: {:?}", self);
+
+        // ctx.text("taa".to_owned());
+    }
+}
+
+impl Actor for WsConn {
+
     type Context = WebsocketContext<Self>;
 }
 
-/// Handler for ws::Message message
-impl StreamHandler<Result<Message, ProtocolError>> for MyWs {
+// Handler for ws::Message message
+impl StreamHandler<Result<Message, ProtocolError>> for WsConn {
 
     // On socket connect
-    fn started(&mut self, _ctx: &mut Self::Context) {
-        println!("Connected");
+    fn started(&mut self, ctx: &mut Self::Context) {
+
+        let socket = self;
+
+        println!("socket connect: {}", socket.id);
+
+        let mut socketlist: Vec<WsConn> = Vec::new();
+
+        socketlist.push(socket.clone());
+
+        socket.send();
+
+        println!("socketlist: {:?}", socketlist);
+
+        let address = ctx.address();
+
+        println!("address: {:?}", address);
     }
 
     // On socket disconnect
     fn finished(&mut self, _ctx: &mut Self::Context) {
-        println!("Disconnected");
+
+        let socket = self;
+
+        println!("socket disconnect: {}", socket.id);
     }
     
     // Handle socket
     fn handle(&mut self, msg: Result<Message, ProtocolError>, ctx: &mut Self::Context) {
-        println!("{:#?}", msg);
+
+        // println!("{:#?}", msg);
         match msg {
             Ok(Message::Ping(msg)) => ctx.pong(&msg),
             Ok(Message::Text(text)) => {
@@ -38,6 +80,7 @@ impl StreamHandler<Result<Message, ProtocolError>> for MyWs {
 }
 
 pub async fn main(req: Request, stream: actix_web::web::Payload) -> Result<Response, Error> {
-    let resp = start(MyWs {}, &req, stream);
-    return resp;
+
+    // Response to websocket connection
+    return actix_web_actors::ws::start(WsConn::new(), &req, stream);
 }
